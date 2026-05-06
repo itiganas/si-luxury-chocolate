f19bcbaf841cffe60898fcb9b115eadad0e31eae
Ina Tiganas <inna.tiganas@gmail.com>
2026-05-06 12:25:43 +0200

    Added header and footer html files

---
+++ script.js
@@ -0,0 +1,13 @@
+// Function to load HTML components
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading ' + file, error));
+}
+
+// Call the function for both header and footer
+loadComponent('header-placeholder', 'header.html');
+loadComponent('footer-placeholder', 'footer.html');
